a6f1512a0bbd91c10c34aecb3e2f99ca898b9a91
fizzyfog <idrissisarah@gmail.com>
2024-01-05 11:22:55 +0100

    Hotfixes

--- opcode_push.c
+++ opcode_push.c
@@ -9,40 +9,39 @@
 
 void push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *new_stack_node;
-	char *next_token;
+	stack_t *new_node;
+	char *token;
 
-	int number_to_push;
+	int n;
 
-	next_token = strtok(NULL, " \n\t");
-	if (next_token == NULL || strchr(next_token, '.') != NULL ||
-		strchr(next_token, ',') != NULL)
+	token = strtok(NULL, " \n\t");
+	if (token == NULL || strchr(token, '.') != NULL || strchr(token, ',') != NULL)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	number_to_push = atoi(next_token);
-	/*printf("token [%s] n [%d]\n", next_token, number_to_push);*/
-	if (number_to_push == 0 && strcmp(next_token, "0") != 0)
+	n = atoi(token);
+	/*printf("token [%s] n [%d]\n", token, n);*/
+	if (n == 0 && strcmp(token, "0") != 0)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	new_stack_node = malloc(sizeof(stack_t));
-	if (new_stack_node == NULL)
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
-	new_stack_node->n = number_to_push;
-	new_stack_node->prev = NULL;
-	new_stack_node->next = *stack;
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *stack;
 
 	if (*stack != NULL)
-		(*stack)->prev = new_stack_node;
+		(*stack)->prev = new_node;
 
-	*stack = new_stack_node;
+	*stack = new_node;
 }
