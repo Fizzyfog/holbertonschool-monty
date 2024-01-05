c31aa2fc75fd22f168dc543110a8d192e7f1380a
fizzyfog <idrissisarah@gmail.com>
2024-01-05 11:14:28 +0100

    Update header file

--- monty.h
+++ monty.h
@@ -1,52 +1,50 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef MAIN__H
+#define MAIN__H
 
-#include <stdlib.h>
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <ctype.h>
 
-/* Structures */
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
- */
+* struct stack_s - doubly linked list representation of a stack (or queue)
+* @n: integer
+* @prev: points to the previous element of the stack (or queue)
+* @next: points to the next element of the stack (or queue)
+*
+* Description: doubly linked list node structure
+* for stack, queues, LIFO, FIFO
+*/
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+		int n;
+
+		struct stack_s *prev;
+		struct stack_s *next;
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
- */
+* struct instruction_s - command and its function
+* @command: the command
+* @fct: function to handle the command
+*
+* Description: command and its function
+* for stack, queues, LIFO, FIFO
+*/
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+		char *command;
+
+		void (*fct)(stack_t **stack, unsigned int line_number);
+
 } instruction_t;
 
-/* function prototypes */
+/* Prototypes */
 
-void opcode_push(stack_t **stack, unsigned int line_number);
-void opcode_pall(stack_t **stack, unsigned int line_number);
-void opcode_pint(stack_t **stack, unsigned int line_number);
-void opcode_pop(stack_t **stack, unsigned int line_number);
-void opcode_swap(stack_t **stack, unsigned int line_number);
-void opcode_add(stack_t **stack, unsigned int line_number);
-void opcode_nop(stack_t **stack, unsigned int line_number);
+void read_file(char *file, stack_t **stack);
+void parse_command(stack_t **stack, char *user_cmd, unsigned int line_number);
 
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
 
-#endif /* MONTY_H */
+#endif
